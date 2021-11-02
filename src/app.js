c03721dfb31cf7c1b5fe9166e9626f9603e05920
KevinAndSH <kevin@uninorte.edu.co>
2021-11-01 22:04:52 -0500

    configuración para uso de métodos HTTP

--- src/app.js
+++ src/app.js
@@ -2,19 +2,27 @@ const express = require("express")
 const app = express()
 const path = require("path")
 const rutas = require("./routes/home.routes.js")
+const methodOverride = require("method-override")
 
 let port = process.env.PORT || 3000
 
-//Definir el motor de templates y la carpeta views
+//* Definir el motor de templates y la carpeta views
 app.set("view engine", "ejs")
 app.set("views", __dirname + "/views")
 
-//Definir la carpeta pública
+//* Definir la carpeta pública
 app.use(express.static(path.join(__dirname, "./public")))
 
-//Requerir las rutas
+//* Requerir las rutas
 app.use("/", rutas)
 
+//* Configurando el entorno para que éste pueda recibir datos por POST
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
-//!FAVOR NO AGREGAR CÓDIGO DEBAJO DE ESTA LINEA
+//* Configurar la librería requerida para usar los métodos PUT y DELETE
+app.use(methodOverride("_method"))
+
+
+//! FAVOR NO AGREGAR CÓDIGO DEBAJO DE ESTA LINEA
 app.listen(port, () => console.log("Servidor corriendo en el puerto", port))
