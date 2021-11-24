c1024d0b3028c34c3e4f9b051d37f5b4bf0ac24c
KevinAndSH <kevin@uninorte.edu.co>
2021-11-23 21:31:42 -0500

    cambios en el form de edición (roto)

--- src/controller/products.controller.js
+++ src/controller/products.controller.js
@@ -35,13 +35,14 @@ const controller = {
   },
   
   update: (req, res) => {
-    let id = parseInt(req.params.id)
-    let product = req.body
-    let fileName = req.file.filename || null
+    res.send(req.body)
+    // let id = parseInt(req.params.id)
+    // let product = req.body
+    // let fileName = req.file.filename || null
     
-    productsModel.editProduct(id, product, fileName)
+    // productsModel.editProduct(id, product, fileName)
 
-    res.redirect("/products")
+    // res.redirect("/products")
   },
   
   delete: (req, res) => {
