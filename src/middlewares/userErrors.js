9f8c2f65ead55d5b34485bc9ac2091346c1c42fe
KevinAndSH <kevin@uninorte.edu.co>
2021-12-29 16:20:59 -0500

    campo de confirmación de contraseña y su respectiva validación fueron añadidos al formulario de registro

--- src/middlewares/userErrors.js
+++ src/middlewares/userErrors.js
@@ -14,7 +14,16 @@ let errors = [
 
   check("password")
     .isEmpty().withMessage("Ingresa una contraseña").bail()
-    .isLength({ min }).withMessage(`Tu contraseña debe tener al menos ${min} caracteres de longitud`)
+    .isLength({ min }).withMessage(`Tu contraseña debe tener al menos ${min} caracteres de longitud`),
+
+  check("password-confirm").custom((value, {req}) => {
+    //* Si las contraseñas no coinciden, se genera un nuevo error
+    if (value !== req.body.password)
+      throw new Error("Passwords don't match")
+
+    //* Se retorna true para indicar el éxito de este custom validator
+    return true
+  }).withMessage("Contraseñas no coinciden")
 ]
 
 
