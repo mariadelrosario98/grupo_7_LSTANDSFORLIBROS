7d59b5f80c05f03db864e91a5338fed7e9ebdd70
KevinAndSH <kevin@uninorte.edu.co>
2022-01-15 15:47:12 -0500

    corrección en data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -1,23 +1,23 @@
 USE `l_stands_for_libros`;
 
-INSERT INTO `users` (`id`,`img_path`, `first_name`, `last_name`, `category`, `email`, `password`)
-VALUES (1, DEFAULT, "Cosme", "Fulanito", "vendor", "cosme@fulanito.com", "$2a$10$g3JINQRgBVzbeSVZtdMjbuj9cBVEJPmOhh1yXtYbO37tVUjknNV1u"),
-(2, DEFAULT,"Maria", "Castro", "Vendor", "maria.castrico@gmail.com", "$2a$10$8l142Fw168WDdqInccjeNe8uOI1YaqNgwlZCN081obw1eU5Omw37O"),
-(3, DEFAULT,"Kevin", "Simanca", "Vendor", "kevin.simanca@gmail.com", "$2a$10$tsnwAebeW3wopjurMBCQg.9uTyzFxQ5LiqQ7mVHbFTe.5yZPwzYjy"),
-(4, DEFAULT,"Elisaul", "García", "Vendor", "elisaul.garcia@gmail.com", "$2a$10$9gdQSCWUe4i2x.2OZfPMeux9naSEZMorTX5NFub0nRW.6wiUDDHJ."),
-(5, DEFAULT,"Zabdiel", "Blanco", "buyer", "zabdiel.blanco@gmail.com", "$2a$10$CJ9qa2nbngu5K5H5aR7NZu/Hi9sBTW7Fb1nVUA23Fk4RJYAn9OvSK"),
-(6, DEFAULT,"Cosme", "Fulanito", "Vendor", "grhz@fulfhsfd.com", "$2hghrshthVzbeSVZtdMjbuj9cBVEJPmOhh1yXtYbO37tVUjknNV1u"),
-(7, DEFAULT,'Luís','Gonçalves', "Vendor", "vefr@fahhs.com", "$2a$10$g3JINQRgBVzfdgsfcBVEJPmOhh1yXtYbO37tVUjknNV1u"),
-(8, DEFAULT,'Bjørn','Hansen', "Vendor", "coahafdh@gsgad.com", "$2a$10$g3JINQRgBVzbeSVZtdMjfgtmOhh1yXtYbO37tVUjknNV1u");
+INSERT INTO `users` (`img_path`, `first_name`, `last_name`, `category`, `email`, `password`)
+VALUES (DEFAULT, "Cosme", "Fulanito", "vendor", "cosme@fulanito.com", "$2a$10$g3JINQRgBVzbeSVZtdMjbuj9cBVEJPmOhh1yXtYbO37tVUjknNV1u"),
+(DEFAULT,"Maria", "Castro", "Vendor", "maria.castrico@gmail.com", "$2a$10$8l142Fw168WDdqInccjeNe8uOI1YaqNgwlZCN081obw1eU5Omw37O"),
+(DEFAULT,"Kevin", "Simanca", "Vendor", "kevin.simanca@gmail.com", "$2a$10$tsnwAebeW3wopjurMBCQg.9uTyzFxQ5LiqQ7mVHbFTe.5yZPwzYjy"),
+(DEFAULT,"Elisaul", "García", "Vendor", "elisaul.garcia@gmail.com", "$2a$10$9gdQSCWUe4i2x.2OZfPMeux9naSEZMorTX5NFub0nRW.6wiUDDHJ."),
+(DEFAULT,"Zabdiel", "Blanco", "buyer", "zabdiel.blanco@gmail.com", "$2a$10$CJ9qa2nbngu5K5H5aR7NZu/Hi9sBTW7Fb1nVUA23Fk4RJYAn9OvSK"),
+(DEFAULT,"Cosme", "Fulanito", "Vendor", "grhz@fulfhsfd.com", "$2hghrshthVzbeSVZtdMjbuj9cBVEJPmOhh1yXtYbO37tVUjknNV1u"),
+(DEFAULT,'Luís','Gonçalves', "Vendor", "vefr@fahhs.com", "$2a$10$g3JINQRgBVzfdgsfcBVEJPmOhh1yXtYbO37tVUjknNV1u"),
+(DEFAULT,'Bjørn','Hansen', "Vendor", "coahafdh@gsgad.com", "$2a$10$g3JINQRgBVzbeSVZtdMjfgtmOhh1yXtYbO37tVUjknNV1u");
 
-INSERT INTO `authors` (`id`, `name`) 
-VALUES (1,'Mr.Happy'),(2,'AC/DC'),(3,'Accept'),(4,`Aerosmith'),(5,'Alanis Morissette'),(6,'Alice In Chains'),(7,'Antônio Carlos Jobim');
+INSERT INTO `authors` (`name`) 
+VALUES ('Mr.Happy'),('AC/DC'),('Accept'),(`Aerosmith`),('Alanis Morissette'),('Alice In Chains'),('Antônio Carlos Jobim');
 
-INSERT INTO `products` (`id`,`img_path`, `name`, `isbn`, `price`, `description`) 
-VALUES (1,DEFAULT,'Happy ever after',12345679910, 45000, "like it");
+INSERT INTO `products` (`img_path`, `name`, `author_id`, `isbn`, `genre_id`, `house_id`, `price`, `description`) 
+VALUES (DEFAULT,'Happy ever after', 1, 12345679910, 1, 1, 45000, "like it");
 
-INSERT INTO `houses` (`id`,`name`) 
-VALUES (1,"Penguin Random House");
+INSERT INTO `houses` (`name`) 
+VALUES ("Penguin Random House");
 
 INSERT INTO `genres` (`name`) 
-VALUES (1, "Romance");
+VALUES ("Romance");
