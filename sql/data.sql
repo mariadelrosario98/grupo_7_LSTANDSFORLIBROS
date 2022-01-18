52045fba7e6d2b4cebc3e227fe5b9a87f2ff98b0
KevinAndSH <kevin@uninorte.edu.co>
2022-01-17 19:45:07 -0500

    mas cambios en data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -11,22 +11,23 @@ VALUES (DEFAULT, "Cosme", "Fulanito", "vendor", "cosme@fulanito.com", "$2a$10$g3
 (DEFAULT,'Bjørn','Hansen', "Vendor", "coahafdh@gsgad.com", "$2a$10$g3JINQRgBVzbeSVZtdMjfgtmOhh1yXtYbO37tVUjknNV1u");
 
 INSERT INTO `authors` (`name`) 
-VALUES ('Mr.Happy'),('AC/DC'),('Accept'),('Alanis Morissette'),('Alice In Chains'),('Antônio Carlos Jobim'), ('Jeannie Lamburn'),('Roarke Pointing'),('Ignaz Woller'),('Archibald Habbema');
+VALUES ("Mark R. Levin"), ("Michelle Zauner"), ("Tara Westover"), ("Colson Whitehead"), ("Louise Penny & Hillary Rodham Clinton"), ("Emily Henry"), ("Bob Woodward & Robert Costa"), ("Mark Manson"), ("Anthony Bourdain & Laurie Woolever"), ("Adam H. Grant");
 
 INSERT INTO `houses` (`name`) 
-VALUES ("Penguin Random House"), ('Dabvine'), ('Tagfeed'), ('Mymm'), ('LiveZ'), ('Voomm'), ('Kimia'), ('Topicblab'), ('Gabspot'), ('Podcat'), ('Yambee'), ('Dabfeed'), ('Jayo'), ('Yacero'), ('Realbuzz'), ('Digitube'), ('Trudeo'), ('Brainbox'), ('Eire'), ('Edgeblab'), ('Yamia');
+VALUES ("Penguin Random House"), ("Dabvine"), ("Tagfeed"), ("Mymm"), ("LiveZ"), ("Voomm"), ("Kimia"), ("Topicblab"), ("Gabspot"), ("Podcat"), ("Yambee"), ("Dabfeed"), ("Jayo"), ("Yacero"), ("Realbuzz"), ("Digitube"), ("Trudeo"), ("Brainbox"), ("Eire"), ("Edgeblab"), ("Yamia");
 
 INSERT INTO `genres` (`name`) 
 VALUES ('Drama|Thriller'), ('Comedy|Horror|Musical|Mystery|Romance'), ('Drama'), ('Western'), ('Comedy'), ('Comedy'), ('Action|Comedy|Horror|Thriller'), ('Comedy|Drama'), ('Action|Adventure|Fantasy'), ('Drama'), ('Drama'), ('Drama'), ('Crime|Drama|Thriller'), ('Crime|Drama'), ('Drama|Sci-Fi'), ('Horror'), ('Comedy|Drama|Romance'), ('Comedy'), ('Action|Comedy|Western'), ('Crime|Horror|Thriller');
 
 INSERT INTO `products` (`img_path`, `name`, `author_id`, `isbn`, `genre_id`, `house_id`, `price`, `description`) 
-VALUES ("american_marxism.png", "American Marxism", "Mark R. Levin", "9781501135972", Genre, House, 50000)
-("cryinginhmart.jpeg", "Crying in H Mart", "Michelle Zauner", "9780525657743", Genre, House, 70500)
-("educated.jpeg", "Educated: A Memoir", "Tara Westover", "9780099511021", Genre, House, 30000)
-("harlem_shuffle.jpeg", "Harlem Shuffle", "Colson Whitehead", "0385545134", Genre, House, 9000)
-("Hillary_clinton.png", "State of Terror", "Louise Penny & Hillary Rodham Clinton", "9783749903276", Genre, House, 20000)
-("people_we_meet.png", "People We Meet on Vacation", "Emily Henry", "9781984806758", Genre, House, 30000)
-("peril.jpg", "Peril", "Bob Woodward & Robert Costa", "9781982182915", Genre, House, 70500)
-("theartofnot.jpg", "The Subtle Art of Not Giving a F*ck", "Mark Manson", "9780062641540", Genre, House, 23200)
-("theworldtravel.jpg", "World Travel: An Irreverent Guide", "Anthony Bourdain & Laurie Woolever", "9781526630216", Genre, House, 90000)
-("think_again.jpeg", "Think Again: The Power of Knowing What You Don't Know", "Adam H. Grant", "9780593298749", Genre, House, 65000)
+VALUES ("american_marxism.png", "American Marxism", 1, "9781501135972", 1, 1, 50000),
+("cryinginhmart.jpeg", "Crying in H Mart", 2, "9780525657743", 2, 2, 70500),
+("educated.jpeg", "Educated: A Memoir", 3, "9780099511021", 3, 3, 30000),
+("harlem_shuffle.jpeg", "Harlem Shuffle", 4, "0385545134", 4, 4, 9000),
+("Hillary_clinton.png", "State of Terror", 5, "9783749903276", 5, 5, 20000),
+("people_we_meet.png", "People We Meet on Vacation", 6, "9781984806758", 6, 6, 30000),
+("peril.jpg", "Peril", 7, "9781982182915", 7, 7, 70500),
+("theartofnot.jpg", "The Subtle Art of Not Giving a F*ck", 8, "9780062641540", 8, 8, 23200),
+("theworldtravel.jpg", "World Travel: An Irreverent Guide", 9, "9781526630216", 9, 9, 90000),
+("think_again.jpeg", "Think Again: The Power of Knowing What You Don't Know", 10, "9780593298749", 10, 10, 65000)
+
