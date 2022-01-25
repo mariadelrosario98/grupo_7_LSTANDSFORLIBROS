117a8f9d05e85605d1054dc83478c564c2fddaac
mariadelrosario98 <md.castro@uniandes.edu.co>
2022-01-24 21:34:41 -0500

    intento conexion BD

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,12 +2,12 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "1234",
-    "database": "l_stands_for_libros",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql",
-    "port": "3307"
+    "port": process.env.DB_PORT
   },
   "test": {
     "username": "root",
