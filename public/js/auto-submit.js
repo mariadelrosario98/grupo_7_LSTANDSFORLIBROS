faa46a853dd3e7c4ee05a5173cb77d3edecff231
KevinAndSH <kevin@uninorte.edu.co>
2022-02-15 13:19:52 -0500

    validación de foto de perfil en frontend en funcionamiento

--- public/js/auto-submit.js
+++
@@ -1,5 +0,0 @@
-const input = document.querySelector(`input[type = "file"]`)
-
-input && input.addEventListener("input", e => {
-  form.submit()
-})
--- public/js/validations/users/image.js
+++ public/js/validations/users/image.js
@@ -1,16 +1,17 @@
 const form = document.querySelector("form.image-container")
 const imageInput = document.getElementById('profile_pic');
-let allowedExtensions = ['jpeg', 'jpg', 'png', 'gif'];
+const feedbackEl = document.querySelector(".feedback")
+
+const allowedExtensions = ['jpeg', 'jpg', 'png', 'gif'];
 
 const validImage = () => {
   let extension = imageInput.value.split('.').pop().toLowerCase()
   return allowedExtensions.includes(extension);
 }
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  if(!validImage){
-    document.querySelector(".feedback").innerText="Tu imagen debe ser de las siguientes extensiones: jpeg, jpg, png o gif" 
+imageInput.addEventListener("input", e => {
+  if(!validImage()){
+    feedbackEl.innerText="Tu imagen debe ser de las siguientes extensiones: jpeg, jpg, png o gif" 
   }
   else{
     form.submit()
