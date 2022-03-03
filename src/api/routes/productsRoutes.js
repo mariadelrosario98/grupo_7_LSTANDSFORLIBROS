830a5f7acfc77fa13583c262b6522f251f6dd351
KevinAndSH <kevin@uninorte.edu.co>
2022-03-03 10:16:47 -0500

    implementación de middleware CORS

--- src/api/routes/productsRoutes.js
+++ src/api/routes/productsRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const router = express.Router()
+const cors = require("cors")
 const { getProducts, getProduct } = require("../controller/productsController")
+
+router.use(cors())
 
 router.get("/", getProducts)
 router.get("/:id", getProduct)
--- src/api/routes/usersRoutes.js
+++ src/api/routes/usersRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const router = express.Router()
+const cors = require("cors")
 const { getUsers, getUser } = require("../controller/usersController")
+
+router.use(cors())
 
 router.get("/", getUsers)
 router.get("/:id",getUser)
